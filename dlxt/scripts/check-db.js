147ff965ec7e0e9f53f20c80db9ec619697fb02e
Liubin-288 <vip288lb@163.com>
2026-07-11 22:50:12 +0800

    Delete dlxt directory

--- dlxt/scripts/check-db.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-(async () => {
-  const models = await p.cableModel.findMany({ select: { name: true, displayName: true, category: true, _count: { select: { specs: true } } } });
-  console.log('Cable Models in DB:', JSON.stringify(models, null, 2));
-  await p.$disconnect();
-})().catch(e => { console.error('ERR:', e.message); process.exit(1); });
